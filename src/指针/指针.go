b3166fb60692954e52dc2635d556167d02a9d6b1
xujian <xujian@flashhold>
2020-07-20 14:05:46 +0800

    day day push!

--- src/指针/指针.go
+++ src/指针/指针.go
@@ -5,14 +5,17 @@ import "fmt"
 func main() {
 	a := 100
 	ptr := &a
-	pptr := & ptr
-	ppptr := & pptr
-	pppptr := & ppptr
-	fmt.Println(a, &a)
+	pptr := &ptr
+	ppptr := &pptr
+	pppptr := &ppptr
+	fmt.Println(&a)
 	fmt.Printf("%T %T %T %T \n", ptr, pptr, ppptr, pppptr)
 
 	// 指针取值
 	c := 100
 	cptr := &c
 	fmt.Println(*cptr)
+
+	d := []int{1, 2, 3}
+	fmt.Printf("%T \n", &d)
 }
